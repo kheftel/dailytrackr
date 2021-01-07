4a69d1ba51c59fc1ecf011225e01d43bbc52fd36
Kawika Heftel <kawika@heftelstudios.com>
2021-01-06 17:43:45 -0700

    chore: update prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
   production: true,
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  firebase: {
+    apiKey: "AIzaSyBWKfwQav4HdEd0xwbKdIi03r0iA-O4mvk",
+    authDomain: "dailytrackr.firebaseapp.com",
+    projectId: "dailytrackr",
+    storageBucket: "dailytrackr.appspot.com",
+    messagingSenderId: "486370993385",
+    appId: "1:486370993385:web:5b3f295db9155446e5fc78",
+    measurementId: "G-4G6QQXKHQF",
+  },
 };
